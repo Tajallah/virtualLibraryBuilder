9b5a25305876e430652c743091ce8b4eebbd9eb2
Taj <tajallah.sims@gmail.com>
2021-09-18 21:57:30 -0700

    Made it asynchronous for faster downloading. Does up to 8 threads at a time.

--- main.py
+++ main.py
@@ -1,37 +1,15 @@
+from concurrent.futures import ThreadPoolExecutor
 import requests
 import wget
 import os
 from bs4 import BeautifulSoup as bsp
 
-#define the root page
-topAddr = "https://openai.com/blog/tags/milestones/"
-html = requests.get(topAddr).text
-soupBowl = bsp(html)
+executor = ThreadPoolExecutor(max_workers=8)
 
-blog = soupBowl.find_all('a')
-leadHolder = []
-for link in blog:
-    newlink = link.get("href")
-    if "blog" in newlink:
-        leadHolder.append(newlink)
-
-papers=[]
-for link in leadHolder:
-    newAddr = "https://openai.com" + link
-    html = requests.get(newAddr).text
-    newBowl = bsp(html).find_all('a')
-    for link in newBowl:
-        newlink = link.get("href")
-        if newlink is not None and "arxiv" in newlink:
-            papers.append(newlink)
-
-if os.path.exists("saved"):
-    if os.path.isdir("saved"):
-        print("--saved directory exists--")
-    else:
-        os.mkdir("saved")
-else:
-    os.mkdir("saved")
+def fetchURL(url):
+    print("Fetching %s"%(url))
+    t = requests.get(url).text
+    return t
 
 def downloadPaper(url):
     urlobj = url.split("/")
@@ -48,6 +26,34 @@ def downloadPaper(url):
     else:
         print("%s already exists"%(filename))
 
-for link in papers:
-    downloadPaper(link)
-    print("Ok we're done here.")
+def getPapers(link):
+    ppr = None
+    newAddr = "https://openai.com" + link
+    html = requests.get(newAddr).text
+    newBowl = bsp(html, features="html.parser").find_all('a')
+    for link in newBowl:
+        newlink = link.get("href")
+        if newlink is not None and "arxiv" in newlink:
+            if os.path.exists("saved"):
+                executor.submit(downloadPaper, newlink)
+            else:
+                os.mkdir("saved")
+                executor.submit(downloadPaper, newlink)
+            
+
+#define the root page
+topAddr = "https://openai.com/blog/tags/milestones/"
+html = fetchURL(topAddr)
+soupBowl = bsp(html, features="html.parser")
+
+blog = soupBowl.find_all('a')
+leadHolder = []
+for link in blog:
+    newlink = link.get("href")
+    if "blog" in newlink:
+        leadHolder.append(newlink)
+
+for link in leadHolder:
+    getPapers(link)
+
+executor.shutdown(wait=True)
